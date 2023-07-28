7186cdf31b7a4b37ee54e3f7cc66226d37f7533c
Nilo <niloleyva@yahoo.com>
2023-07-28 19:30:57 -0400

    New Assets + FPS Control Script + Gun and bullet Script

---
+++ Assets/Scripts/Guns/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private bool moveBullet;
+    private float speed = 45;
+
+    private float deleteTime = 1f;
+    private float timeTillDeletion;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        timeTillDeletion += Time.deltaTime;
+        MoveForward();
+    }
+
+    // Update is called once per frame
+    private void MoveForward()
+    {
+        transform.position += transform.forward * (speed * Time.deltaTime);
+        
+        if(timeTillDeletion >= deleteTime) Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Guns/Pistol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject gunBarrel;
+
+    [SerializeField] private float fireRate = 0.1f;
+
+    private int clipSize = 20;
+    private float timeToFire;
+
+    private void FireBullet()
+    {
+        var newBullet = Instantiate(bullet, gunBarrel.transform.position + new Vector3(0, 0, 0.5f), gunBarrel.transform.rotation);
+        timeToFire = 0;
+    }
+    
+    private void Update()
+    {
+        timeToFire += Time.deltaTime;
+
+        if (timeToFire < fireRate) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireBullet();
+        }
+    }
+
+}
